57bc6e16f5c0fb413b8dd3374e010a585933b6eb
MT <25986959+mtoprak53@users.noreply.github.com>
2025-04-15 11:51:44 -0400

    2nd commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { shadow } from "@/styles/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import DarkModeToggle from "./DarkModeToggle";
+import LogoutButton from "./LogoutButton";
+
+function Header() {
+  // const user = null;
+  const user = "mete";
+  return (
+    <header className="relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+      style={{
+        boxShadow: shadow
+      }}>
+      <Link className="flex items-end gap-2" href="/">
+        <Image
+          src="/kurt.png"
+          alt="METE Notes Logo"
+          width={60}
+          height={60}
+          className="rounded-full"
+          priority
+        />
+
+        <h1 className="flex flex-col text-2xl font-semibold leading-6">
+          METE <span>Notes</span>
+        </h1>
+      </Link>
+
+      <div className="flex gap-4">
+        {user ? (
+          <LogoutButton />
+        ) : (
+          <>
+          <Button asChild>
+            <Link href="/sign-up" className="hidden sm:block">
+              Sign Up
+            </Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/login">
+              Login
+            </Link>
+          </Button>
+          </>
+        )}
+        <DarkModeToggle />
+      </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { toast } from "sonner"
+import { useRouter } from "next/navigation";
+
+
+
+function LogoutButton() {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleLogOut = async () => {
+    setLoading(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // const errorMessage = null;
+    const errorMessage = "error when logging out !!";
+
+    if (!errorMessage) {
+      toast.success("Logged out", {
+        description: "You have been logged out successfully",
+      });
+      router.push("/");
+    } else {
+      toast.error("Error", {
+        description: errorMessage,
+      });
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <Button
+      variant="outline" 
+      onClick={handleLogOut}
+      disabled={loading}
+      className="w-24"
+    >
+      { loading ? <Loader2 className="animate-spin" /> : "Log Out" }
+    </Button>
+  )
+}
+
+export default LogoutButton
+
