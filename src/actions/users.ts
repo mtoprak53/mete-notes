7096e1d4bb5f3edbb1177d2bc9ec14b7db600e27
MT <25986959+mtoprak53@users.noreply.github.com>
2025-05-01 16:46:09 -0400

    built homepage

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { createClient } from "@/auth/server";
+import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
 export const loginAction = async (email: string, password: string) => {
@@ -49,6 +50,12 @@ export const signUpAction = async (email: string, password: string) => {
     if (!userId) throw new Error("Error signing up");
 
     // add user to database
+    await prisma.user.create({
+      data: {
+        id: userId, 
+        email, 
+      }
+    })
 
     return { errorMessage: null };
   } catch (error) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { getUser } from "@/auth/server";
+import AskAIButton from "@/components/AskAIButton";
+import { prisma } from "@/db/prisma";
 
-function Homepage() {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+async function Homepage({ searchParams }: Props) {
+
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
+  const noteId = Array.isArray(noteIdParam)
+    ? noteIdParam![0]
+    : noteIdParam || "";                              
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId, authorId: user?.id },
+  });
+
   return (
-    <div 
-      // className='h-20 bg-red-400 text-blue-300'
-    >
-      Homepage
+    <div className="flex h-full flex-col items-center gap-4">
+      <div className="flex w-full max-w-4xl justify-end gap-2">
+        <AskAIButton user={user} />
+        <NewNoteButton user={user} />
+      </div>
+
+      <NoteTextInput noteId={noteId} startingNoteText={note?.text || ""} />
     </div>
   )
 }
